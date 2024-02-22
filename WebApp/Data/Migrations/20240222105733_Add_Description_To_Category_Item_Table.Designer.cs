5d0a25825dd179944bcd4e8e648b5264f49687ac
Amer Becic <amer.becic999@gmail.com>
2024-02-22 11:59:32 +0100

    Migrations for CategoryItem EFModel

--- WebApp/Data/Migrations/20240222105733_Add_Description_To_Category_Item_Table.Designer.cs
+++ WebApp/Data/Migrations/20240222105733_Add_Description_To_Category_Item_Table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Data;
 
 namespace WebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240222105733_Add_Description_To_Category_Item_Table")]
+    partial class Add_Description_To_Category_Item_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

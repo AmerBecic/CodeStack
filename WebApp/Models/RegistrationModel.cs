b92a1a6b4fc39b8506bae3bb6c682e6e7de4244b
Amer Becic <amer.becic999@gmail.com>
2024-02-28 09:09:53 +0100

    Setting up Custom Registration Form: Added new _UserRegistrationPartial and RegistrationModel
    Partial View needs to be Designed, currently it's just modal demo

---
+++ WebApp/Models/RegistrationModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Address Line 1")]
+        public string Address1 { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Address Line 2")]
+        public string Address2 { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Post Code")]
+        public string PostCode { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public bool AcceptUserAgreement { get; set; }
+
+        public string RegistrationInValid { get; set; }
+    }
+}

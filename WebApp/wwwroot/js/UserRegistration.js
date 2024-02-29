6a6dc0ffe193eb72f5749eba8b1eebeedb8b6f72
Amer Becic <amer.becic999@gmail.com>
2024-02-29 09:06:21 +0100

    Checking if UserName exists: Added AJAX code in UserRegistration.js
    Top of the form now displays alert if entered email is already in use when email field is out of focus

--- WebApp/wwwroot/js/UserRegistration.js
+++ WebApp/wwwroot/js/UserRegistration.js
@@ -13,6 +13,47 @@
         }
     }
 
+    $("#UserRegistrationModal input[name='Email']").blur(() => {
+
+        var email = $("#UserRegistrationModal input[name='Email']").val();
+
+        console.log(email);
+
+        var url = "UserAuth/UserNameExists?userName=" + email;
+
+        console.log(url);
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: (data) => {
+                if (data == true) {
+                    var errorHTML = '<div class="alert alert-warning alert-dismissible fade show" role="alert">' +
+                        '<strong>Invalid Email</strong><br>This Email address has already been registered' +
+                        '<button type ="button" class="close" data-dismiss="alert" aria-label="Close"' +
+                        '<span aria-hidden="true">&times;</span>' +
+                        '</button>' +
+                        '</div>';
+
+                    $("#alert_placeholder").html(errorHTML);
+
+                    //var modalBody = $(".modal-body");
+
+                    //modalBody.prepend(errorHTML);
+                }
+                else {
+                    $("#alert_placeholder").html("");
+                }
+            },
+            error: (xhr, ajaxOptions, thrownError) => {
+                console.error(thrownError + "/r/n" + xhr.statusText + "/r/n" + xhr.responseText);
+            }
+        });
+
+
+        
+    });
+
     var registerButton = $("#UserRegistrationModal button[name='register']").click(onUserRegisterClick);
 
     function onUserRegisterClick() {
